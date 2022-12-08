2141c89beb0ceebbb186545db834260572d172a3
Danzz Coding <muhammadramdani196453@gmail.com>
2022-12-08 22:52:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function create() {
 	var text = encodeURIComponent(document.getElementById("text").value);
-	document.querySelector('#result').innerHTML = `<img style='max-width: 400px;max-height: 400px;' src='https://danzzapi.xyz/api/maker/nulis?text=${text}&apikey=danzz' alt="nulis.png"></img>`;
+	document.querySelector('#result').innerHTML = `<img style='max-width: 400px;max-height: 400px;' src='https://api.xteam.xyz/magernulis2?text=${text}&APIKEY=af294af2d7b1fcd0' alt="nulis.png"></img>`;
 	}
